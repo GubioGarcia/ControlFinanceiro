c63a046005f69c258b6c0132ecb996d0b0de7882
Gubio Garcia <gubiogarcia@gmail.com>
2024-09-25 15:56:50 -0300

    feat: temas de estilização da aplicação

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
   imports: [
-    MenubarModule
+    MenubarModule,
+    SelectButtonModule
   ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
@@ -14,6 +16,10 @@ import { MenuItem } from 'primeng/api';
 
 export class MenuBarComponent implements OnInit {
   items: MenuItem[] | undefined;
+  temas: any[] | undefined;
+  temaSelecionado: string | undefined;
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     this.items = [
@@ -96,6 +102,29 @@ export class MenuBarComponent implements OnInit {
               }
           ]
         }
+    ];
+
+    this.temas = [
+      {
+        label: 'Claro',
+        icon: 'pi pi-sun',
+        value: 'bootstrap4-light-purple'
+      },
+      {
+        label: 'Escuro',
+        icon: 'pi pi-moon',
+        value: 'bootstrap4-dark-purple'
+      }
     ]
-}
+
+    this.temaSelecionado = 'bootstrap4-light-purple';
+    this.changeTheme(this.temaSelecionado);
+  }
+
+  changeTheme(theme: string) {
+    const themeLink = document.getElementById('theme-link') as HTMLLinkElement;
+    if (themeLink) {
+      this.renderer.setAttribute(themeLink, 'href', `assets/themes/${theme}/theme.css`);
+    }
+  }
 }
